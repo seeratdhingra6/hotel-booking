59500eafee7c15da7567311608b338311464c961
seerat <seerat12mr@gmail.com>
2024-02-28 02:28:38 +0530

    fix footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className='text-center tw-text-white tw-backdrop-blur-md tw-h-11 tw-flex tw-align-middle tw-justify-center'>
-            <p className='my-auto '>© Developed by <span className='fw-medium'>Hasan</span></p>
-        </footer>
-    );
+  return (
+    <footer className="text-center tw-text-white tw-backdrop-blur-md tw-h-11 tw-flex tw-align-middle tw-justify-center">
+      <p className="my-auto ">
+        © Developed by <span className="fw-medium">Seerat Arora</span>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
